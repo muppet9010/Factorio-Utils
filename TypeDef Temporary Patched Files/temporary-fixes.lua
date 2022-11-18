56e4503cdcfb369551ed054743e0b090c509e966
Muppet9010 <mike.hollett@gmail.com>
2022-11-18 01:11:33 +0000

    Sumneko Temporary Fixes
    Minor additions and fixes from WIP on Careful Driver v 1.0.0

--- TypeDef Temporary Patched Files/temporary-fixes.lua
+++ TypeDef Temporary Patched Files/temporary-fixes.lua
@@ -14,18 +14,49 @@ local LuaSurface = {
     get_tile = function(x, y) end,
 }
 
+---@class LuaEntity
+LuaEntity = {
+	--- Is optional parameter. Logged: https://forums.factorio.com/viewtopic.php?f=28&t=97880&p=577348#p577348
+	---@param driver LuaEntity|PlayerIdentification|nil@The new driver or `nil` to eject the current driver if any.
+	set_driver = function (driver) end
+	
+	--- Is optional parameter. Logged: https://forums.factorio.com/viewtopic.php?f=28&t=97880&p=577348#p577348
+	---@param passenger LuaEntity|PlayerIdentification|nil@The new Passenger or `nil` to eject the current driver if any.
+	set_passenger = function (passenger) end
+}
+
+---@class Prototype.Entity
+---@field flags string[] # This is just an array of strngs in data stage. The Factorio API files by Nexula has it wrong.
+---@field icons? IconData[]
+---@field icon_size? int16
+
+---@class IconData
+---@field icon string
+---@field icon_size? int16 # Mandatory for each IconData unless provided at the LuaEntity class level.
+---@field scale? double
+---@field icon_mipmaps? uint8
+---@fie;d shift Vector
+---@field tint Color
+
+---@class RotatedAnimation
+---@field layers Animation[]
+---@field hr_version RotatedAnimation
+
+---@class Animation
+---@field width uint
+---@field frame_count uint
 
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class MinableProperties
 ---@field result string
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class ModifierPrototype
 ---@field type string
 ---@field recipe string
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class Animation
 ---@field draw_as_glow? boolean
 ---@field layers? Animation[]
@@ -33,24 +64,25 @@ local LuaSurface = {
 ---@field filename? string
 ---@field stripes? Stripe[]
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class Sprite
 ---@field hr_version? Sprite
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@alias AnimationVariations Animation|Animation[]
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class Stripe
 ---@field filename? string
 
--- Just add the minimal attributes I need for Utils.
+-- Just add the minimal attributes I need.
 ---@class DamagePrototype
 ---@field type string
 
 
 
----@class RealOrientation
+
+---@class RealOrientation:float
 ---@operator unm:RealOrientation
 ---@operator mod:RealOrientation
 ---@operator add:RealOrientation
