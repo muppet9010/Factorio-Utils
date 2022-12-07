fef5bff22a5aeeecdb77ce7d9aad17e7483da972
Muppet9010 <mike.hollett@gmail.com>
2022-12-07 16:48:34 +0000

    Sumneko type def control template

--- Mod Template Files/control.lua
+++ Mod Template Files/control.lua
@@ -5,6 +5,7 @@ local function OnLoad()
 	--Any Remote Interface registration calls can go in here or in root of control.lua
 end
 
+---@param event EventData.on_runtime_mod_setting_changed|nil # nil value when called from OnStartup (on_init & on_configuration_changed)
 local function OnSettingChanged(event)
     --if event == nil or event.setting == "xxxxx" then
     --	local x = tonumber(settings.global["xxxxx"].value)
